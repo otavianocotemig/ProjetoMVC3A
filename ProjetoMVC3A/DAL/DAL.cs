2ffe9e945267e6cf627b8e460e328cce0fc4b059
otavianocotemig <otaviano@cotemig.com.br>
2020-06-03 08:57:51 -0300

    Criação da Classe DAL

--- ProjetoMVC3A/DAL/DAL.cs
+++ ProjetoMVC3A/DAL/DAL.cs
@@ -1,14 +1,22 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
-
 namespace ProjetoMVC3A.DAL
 {
     class DAL
     {
+        private MySqlConnection conexao;
+        private string string_conexao = "Persist security info= false; " + "server = localhost; " +
+                                         "Database=dbmvc " + " user=root; pwd=;";
+        private void conectar()
+        {
+            conexao = new MySqlConnection(string_conexao);
+            conexao.Open();
+        }                                    
+
     }
 }
--- ProjetoMVC3A/UI/FrmLogin.cs
+++ ProjetoMVC3A/UI/FrmLogin.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ProjetoMVC3A.DAL;
 
 namespace ProjetoMVC3A.UI
 {
@@ -24,7 +26,7 @@ namespace ProjetoMVC3A.UI
 
         private void btnEntrar_Click(object sender, EventArgs e)
         {
-
+           private DAL dao = new DAL();
         }
     }
 }
