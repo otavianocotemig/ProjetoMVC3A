94e81649dffc506bb819083f60a5778b7e15cca6
otavianocotemig <otaviano@cotemig.com.br>
2020-06-05 08:58:18 -0300

    Inclusão da DTO e DAL

--- ProjetoMVC3A/DAL/DALBD.cs
+++ ProjetoMVC3A/DAL/DALBD.cs
@@ -12,11 +12,37 @@ namespace ProjetoMVC3A.DAL
         private MySqlConnection conexao;
         private string string_conexao = "Persist security info= false; " + "server = localhost; " +
                                          "Database=dbmvc " + " user=root; pwd=;";
-        private void conectar()
+        // Metodo para conexão com o BD
+        public void conectar()
         {
-            conexao = new MySqlConnection(string_conexao);
-            conexao.Open();
-        }                                    
+            try
+            {
+                conexao = new MySqlConnection(string_conexao);
+                conexao.Open();
+            }
+            catch (MySqlException e)
+            {
+                throw new Exception("Problemas na conexão com o banco de dados. Erro: " + e.Message);
 
+            }
+        } 
+        // Metodo para Executar Consulta no Banco
+        public void ExecutarComando(string sql)
+        {
+            try
+            {
+                conectar();
+                MySqlCommand comando = new MySqlCommand(sql, conexao);
+                comando.ExecuteNonQuery();
+            }
+            catch (MySqlException e)
+            {
+                throw new Exception("Não foi possível executar a instrução no Banco. Erro: " + e.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
     }
 }
